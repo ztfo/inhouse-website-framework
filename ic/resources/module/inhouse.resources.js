8230827a5319959a1af056f2533eeffeba15abcf
Joshua D. Gill <josh.dgill@gmail.com>
2016-04-12 19:55:07 -0700

    added in support for legacy object based content pages, added in the resource directive, etc

---
+++ ic/resources/module/inhouse.resources.js
@@ -0,0 +1,13 @@
+angular.module('inhouseApp')
+.directive('ihResources', function() {
+	return {
+		templateUrl : function(el, attrs) {
+			return 'ic/resources/template/' + (attrs.config || 's1') + '-inhouse.resources.htm';
+		},
+		restrict: 'E',
+		link: function(scope, el, attrs) {
+			scope.limit = attrs.limit || 3;
+			scope.resources = window.agentSettings.content;
+		}
+	};
+});
