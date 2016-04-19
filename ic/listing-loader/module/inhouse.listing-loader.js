bdfbec334efd9b8e156b0128b3ccb460923fb455
Joshua D. Gill <josh.dgill@gmail.com>
2016-04-18 19:35:09 -0700

    added agent to the listing loader scope

--- ic/listing-loader/module/inhouse.listing-loader.js
+++ ic/listing-loader/module/inhouse.listing-loader.js
@@ -1,6 +1,5 @@
 angular.module('inhouseApp')
 .directive('ihListingLoader', ['$routeParams', '$timeout', '$location', function($routeParams, $timeout, $location) {
-
 	return {
 		scope: {
 			config: '@',
@@ -9,6 +8,9 @@ angular.module('inhouseApp')
 		replace: true,
 		templateUrl : function(el, attrs) {
 			return 'ic/listing-loader/template/' + (attrs.config || 's1') + '-inhouse.listing-loader.htm';
+		},
+		link: function(scope) {
+			scope.agent = window.agentSettings;
 		}
 	};
 }]);
