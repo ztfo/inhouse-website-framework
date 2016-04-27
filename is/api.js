b0d663d37d2468f9e3519b77ae980807bce402b5
Joshua D. Gill <josh.dgill@gmail.com>
2016-04-26 19:06:28 -0700

    flipped back to inhouse api from localhost

--- is/api.js
+++ is/api.js
@@ -1,6 +1,6 @@
 angular.module('inhouseApp')
 .factory('inhouseApi', function($http) {
-	var urlBase = 'http://localhost/inhouse-canvas/api/v1/';
+	var urlBase = 'https://www.getinhouse.io/api/v1/';
 	var inhouseApi = {};
 	var userId = window.agentSettings.userId;
 	var userHash = window.agentSettings.userHash;
