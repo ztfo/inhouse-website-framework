a9bffc29f441e6312cc275f93d0f588b25471e12
Josh Gill <josh.dgill@gmail.com>
2016-05-06 18:52:28 -0700

    listing details directive

---
+++ ic/listing-details/module/inhouse.listing-details.js
@@ -0,0 +1,21 @@
+angular.module('inhouseApp')
+.directive('ihListingDetails', [function (inhouseApi, $timeout) {
+	return {
+		templateUrl : function(el, attrs) {
+			return 'ic/listing-details/template/' + (attrs.config || 's1') + '-inhouse.listing-details.htm';
+		},
+		restrict: 'E',
+		replace: true,
+		scope: {
+			classes: "@classes"
+		},
+		controller: function ($scope) {
+		},
+		link: function (scope, element, attrs) {
+			scope.$on('listingLoaded', function (event, args) {
+				scope.listing = args;
+			});
+		}
+	};
+}]);
+
