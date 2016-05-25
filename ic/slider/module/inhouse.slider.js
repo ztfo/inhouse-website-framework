5ce18ebd1198d90e7a74ee881f4ebb28989f3a38
Josh Gill <josh.dgill@gmail.com>
2016-05-24 20:07:52 -0700

    modified slider to do a direct api pull

--- ic/slider/module/inhouse.slider.js
+++ ic/slider/module/inhouse.slider.js
@@ -1,5 +1,5 @@
 angular.module('inhouseApp')
-.directive('ihSlider', function() {
+.directive('ihSlider', ['inhouseApi', function(inhouseApi) {
 	return {
 		templateUrl : function(el, attrs) {
 			return 'ic/slider/template/' + (attrs.config || 's1') + '-inhouse.slider.htm';
@@ -8,20 +8,32 @@ angular.module('inhouseApp')
 		replace: true,
 		scope: {
 			slider: '@',
-			classes: '@'
+			classes: '@',
+			pull: '@',
+			nodefault: '@nodefault'
 		},
 		controller: function($scope) {
 			$scope.LandingComponent = window.storySettings.LandingComponent;
 			$scope.agent = window.agentSettings;
 		},
 		link: function(scope, element, attrs) {
-			scope.$on('storyLoaded', function(event, args) {
-				scope.slides = args[scope.slider].slides;
-				element.carousel({
-                  pause: "false",
-                  interval: 25000
-                });
-			});
+			if(scope.pull !== undefined) {
+				inhouseApi.getData({resource: 'slider', slider: scope.slider}).success(function(response) {
+					scope.slides = response.response.slides;
+					element.carousel({
+						pause: "false",
+						interval: 25000
+					});
+				});
+			} else {
+				scope.$on('storyLoaded', function(event, args) {
+					scope.slides = args[scope.slider].slides;
+					element.carousel({
+						pause: "false",
+						interval: 25000
+					});
+				});
+			}
 		}
 	};
-});
+}]);
