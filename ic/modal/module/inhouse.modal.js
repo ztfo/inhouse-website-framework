3519fb3e7f9f604d8f43cfab48ae662bfc472e32
Josh Gill <josh.dgill@gmail.com>
2016-05-25 19:55:22 -0700

    modal stuff

---
+++ ic/modal/module/inhouse.modal.js
@@ -0,0 +1,28 @@
+angular.module('inhouseApp')
+.directive('ihModal', [function() {
+	return {
+		restrict: 'E',
+		scope: {
+			config: '@',
+			id: '@',
+			confirm: '@'
+		},
+		template: "<div ng-include='templateUrl()'></div>",
+		link: function(scope, element, attrs) {
+			scope.templateUrl = function() {
+				return 'ic/modal/template/' + (scope.config || 's1') + '-modal.inhouse.htm';
+			}
+			scope.$on('showModal', function(event, args) {
+				scope.modal = args.modal;
+				if(scope.id == args.id) {
+					element.find('.modal').modal('show');
+				}
+			});
+			element.find('.modal').on('bs.modal.hide', function() {
+				scope.$emit('modalClosed', {modal: scope.id});
+			});
+		},
+		controller: function($scope) {
+		}
+	};
+}]);
--- is/controllers/subdivision-controller.js
+++ is/controllers/subdivision-controller.js
@@ -1,5 +1,17 @@
 angular.module('inhouseApp')
 .controller('subdivisionController', ['$scope', 'inhouseApi', '$window', '$routeParams', function($scope, inhouseApi, $window, $routeParams) {
+	$scope.showModal = function(content) {
+		var modal = {};
+		if(typeof $scope.subdivision[content] !== 'undefined') {
+			modal.title = $scope.subdivision[content].title;
+			if(typeof $scope.subdivision[content].content !== 'undefined') {
+				modal.body = $scope.subdivision[content].content;
+			} else if(typeof $scope.subdivision[content].image !== 'undefined') {
+				modal.body = "<img src='" + $scope.subdivision[content].image + "'>";
+			}
+			$scope.$broadcast('showModal', {modal: modal, id: 'subdivModal'});
+		}
+	};
 	if(typeof $window.storySettings.subdivisions !== 'undefined') {
 		var subdivs = $window.storySettings.subdivisions;
 		for (var i = 0; i < subdivs.length; i++) {
