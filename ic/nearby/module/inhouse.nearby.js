79de7b96cbf152f1a727bda46686a62ddd8f2ff5
lepalomares <lepalomares@me.com>
2016-08-15 10:38:32 -0700

    listing component cleanup

--- ic/nearby/module/inhouse.nearby.js
+++ ic/nearby/module/inhouse.nearby.js
@@ -1,15 +1,1 @@
-angular.module('inhouseApp')
-.directive('ihAbout', function() {
-	return {
-		scope: {
-			classes: "@classes"
-		},
-		templateUrl : function(el, attrs) {
-			return 'ic/about/template/' + (attrs.config || 's1') + '-inhouse.about.htm';
-		},
-		restrict: 'E',
-		controller: function($scope) {
-			$scope.agent = window.agentSettings;
-		}
-	};
-});
+// TODO
