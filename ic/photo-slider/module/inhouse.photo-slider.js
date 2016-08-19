b1f72d23906e4d240aeb2ea2aba9071467119260
johnazre <jaboyak@gmail.com>
2016-08-19 14:02:03 -0700

    photo slider added

--- ic/photo-slider/module/inhouse.photo-slider.js
+++ ic/photo-slider/module/inhouse.photo-slider.js
@@ -1,1 +1,12 @@
-// TODO
+angular.module('inhouseApp').directive('ihPhotoSlider', function(){
+  return {
+    restrict: 'E',
+    template: '<ng-include src="theUrl"></ng-include>',
+    link: function(scope, elem, attrs){
+      attrs.config = 'p1'
+    },
+    controller: function($scope){
+      $scope.theUrl = 'ic/photo-slider/template/p1-inhouse.photo-slider.htm';
+    }
+  }
+})
