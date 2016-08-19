3b4a802073916d6509f0edbc12326f455531c5dc
lepalomares <lepalomares@me.com>
2016-08-19 16:55:42 -0700

    p1 theme comps and fluff directives

---
+++ ic/about-listing/module/inhouse.about-listing.js
@@ -0,0 +1,15 @@
+angular.module('inhouseApp')
+.directive('ihAboutListing', function() {
+	return {
+		scope: {
+			classes: "@classes"
+		},
+		templateUrl : function(el, attrs) {
+			return 'ic/about-listing/template/' + (attrs.config || 's1') + '-inhouse.about-listing.htm';
+		},
+		restrict: 'E',
+		controller: function($scope) {
+			$scope.agent = window.agentSettings;
+		}
+	};
+});
