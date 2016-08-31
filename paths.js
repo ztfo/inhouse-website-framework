ad1a35e37b8198b164ea19f8703fbdabacd15902
Luis Palomares <Luis Palomares>
2016-08-30 22:59:10 -0700

    remove glitchy uikit, replaced smoothscroll with better one

--- paths.js
+++ paths.js
@@ -5,7 +5,6 @@ module.exports = {
     "dev/ia/css/bootstrap-slider.min.css",
     "dev/ia/css/owl.carousel.css",
     "dev/ia/css/uikit.css",
-    "dev/ia/css/sticky.min.css",
     "dev/ia/css/slidenav.css",
     "dev/ia/css/inhouse.css",
     "dev/ia/icons/property-icons/ih-icons.css",
