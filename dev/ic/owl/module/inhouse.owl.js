e63b9dd8cca6819c9f725f60f2374bbedfdeedd8
lepalomares <lepalomares@me.com>
2016-09-02 11:16:06 -0700

    added testimonial markup

--- dev/ic/owl/module/inhouse.owl.js
+++ dev/ic/owl/module/inhouse.owl.js
@@ -13,13 +13,13 @@ angular.module('ihframework')
           $scope.renderOwl();
         }
       });
-      
+
       $scope.$watch('length', function(newVal) {
         if(newVal !== undefined && newVal > 0) {
           $scope.renderOwl();
         }
       });
-      
+
       $scope.renderOwl = function() {
         $timeout(function() {
           if($scope.owl != undefined) {
@@ -29,9 +29,10 @@ angular.module('ihframework')
             pause: "true",
             interval: 9999,
             navigation: true,
-            responsive: $scope.responsive
+            responsive: $scope.responsive,
+            navigationText : ["<i class='fa fa-caret-right'></i>","<i class='fa fa-caret-right'></i>"]
           });
-          
+
           $scope.owl = $($element).data('owl.carousel');
           $element.find('.owl-item').click(function() {
             var index = $(this).index('.owl-item');
