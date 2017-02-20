9972f5900264d13da10eb81872e45b8a9a0c49a8
Joshua Gill <josh.dgill@gmail.com>
2017-02-20 20:56:18 +0000

    Merged in IN-42-make-text-fields-editable (pull request #1)
    made amenities editable

--- dev/ic/about-listing/module/inhouse.about-listing.js
+++ dev/ic/about-listing/module/inhouse.about-listing.js
@@ -4,11 +4,26 @@ angular.module('ihframework')
 		scope: {
 			classes: "@classes",
 			listing: '=',
-			config: '='
+			config: '=',
+			data: '=',
 		},
 		template: '<ng-include src="templateUrl" class="{{classes}}"></ng-include>',
 		restrict: 'E',
 		controller: function($rootScope, $scope) {
+			//only save the public remarks so the rest of the listing comes back unscathed
+
+			$scope.$watch('listing.publicRemarks', function(newVal, oldVal) {
+				if(oldVal == undefined) { //first time running this
+					if(typeof $scope.data == 'object' && $scope.data.publicRemarks !== undefined && typeof $scope.listing == 'object') {
+						$scope.listing.publicRemarks = $scope.data.publicRemarks;
+					}
+				} else {
+					if(typeof $scope.data == 'object') {
+						$scope.data.publicRemarks = newVal;
+					}
+				}
+			});
+
 			$scope.$watch('config', function(newVal) {
 				if(newVal !== undefined) {
 					$scope.templateUrl = 'build/templates/ic/about-listing/template/' + $scope.config + '-inhouse.about-listing.html';
