4ee8e77d2b097c949a406f2e32bd9d71eee848f7
Joshua D. Gill <josh.dgill@gmail.com>
2017-03-14 17:36:21 -0700

    updated share links to use new api

--- dev/is/controllers/listing-controller.js
+++ dev/is/controllers/listing-controller.js
@@ -1,90 +1,90 @@
 angular.module('ihframework')
 .controller('listingController', ['$window', '$rootScope', '$scope', '$routeParams', 'inhouseApi', '$timeout', function($window, $rootScope, $scope, $route, inhouseApi, $timeout) {
-	$scope.mls = $route.mls;
-	window.mls = $scope.mls;
-	$('#main-view').addClass('load-overlay');
+  $scope.mls = $route.mls;
+  window.mls = $scope.mls;
+  $('#main-view').addClass('load-overlay');
 
-	if(typeof $rootScope.theWebsiteData.listingConfig !== 'undefined' && $rootScope.theWebsiteData.listingConfig === 's2') {
-		$rootScope.$broadcast('hideFooter');
-		$scope.$on('$destroy', function() {
-			$rootScope.$broadcast('showFooter');
-		});
-	}
+  if(typeof $rootScope.theWebsiteData.listingConfig !== 'undefined' && $rootScope.theWebsiteData.listingConfig === 's2') {
+    $rootScope.$broadcast('hideFooter');
+    $scope.$on('$destroy', function() {
+      $rootScope.$broadcast('showFooter');
+    });
+  }
 
-	$scope.$on('$destroy', function() {
-		//prevent the modal from persisting
-		$('#accountModal').off('hidden.bs.modal');
-		$('#accountModal').modal('hide');
-	});
-	$scope.shareUrl = "https://www.getinhouse.io/share-listing/" + $rootScope.theUserData.userId + '/' + $scope.mls;
-	$scope.getData = function() {
-		if(typeof Storage !== 'undefined') {
-			if(typeof localStorage.inhouseAgentLead !== 'undefined') { //user is signed in already, load the listing
-			} else {
-				if(typeof localStorage.inhouseViewedListings !== 'undefined') { //user isn't signed in yet, and they have viewed listings already
-				if($rootScope.theWebsiteData.disableLeadSignup !== true) {
-					$scope.viewedListings = $.parseJSON(localStorage.inhouseViewedListings);
-					if($scope.viewedListings.indexOf($scope.mls) !== -1) {
-					} else if((localStorage.inhouseSearchFreebies !== 'false' && $scope.viewedListings.length > 2 )|| (localStorage.inhouseSearchFreebies == 'false' && $scope.viewedListings.length > 5)) {
-						$('#accountModal').modal('show');
-						$('#accountModal').off('hidden.bs.modal');
-						$('#accountModal').on('hidden.bs.modal', (function(scope) {
-							return function() {
-								$scope.getData();
-							};
-						})($scope));
-						return;
-					} else { //not at the limit yet. push this mls to the stack
-						$scope.viewedListings.push($scope.mls);
-						localStorage.inhouseViewedListings = JSON.stringify($scope.viewedListings);
-					}
-				}
-			} else { //they haven't viewed any yet, add this to the arr
-			$scope.viewedListings = [$scope.mls];
-			localStorage.inhouseViewedListings = JSON.stringify($scope.viewedListings);
-		}
-	}
+  $scope.$on('$destroy', function() {
+    //prevent the modal from persisting
+    $('#accountModal').off('hidden.bs.modal');
+    $('#accountModal').modal('hide');
+  });
+  $scope.shareUrl = "https://app.getinhouse.io/user/" + $rootScope.theUserData.userId + '/share-listing/' + $scope.mls;
+  $scope.getData = function() {
+    if(typeof Storage !== 'undefined') {
+      if(typeof localStorage.inhouseAgentLead !== 'undefined') { //user is signed in already, load the listing
+      } else {
+        if(typeof localStorage.inhouseViewedListings !== 'undefined') { //user isn't signed in yet, and they have viewed listings already
+        if($rootScope.theWebsiteData.disableLeadSignup !== true) {
+          $scope.viewedListings = $.parseJSON(localStorage.inhouseViewedListings);
+          if($scope.viewedListings.indexOf($scope.mls) !== -1) {
+          } else if((localStorage.inhouseSearchFreebies !== 'false' && $scope.viewedListings.length > 2 )|| (localStorage.inhouseSearchFreebies == 'false' && $scope.viewedListings.length > 5)) {
+            $('#accountModal').modal('show');
+            $('#accountModal').off('hidden.bs.modal');
+            $('#accountModal').on('hidden.bs.modal', (function(scope) {
+              return function() {
+                $scope.getData();
+              };
+            })($scope));
+            return;
+          } else { //not at the limit yet. push this mls to the stack
+            $scope.viewedListings.push($scope.mls);
+            localStorage.inhouseViewedListings = JSON.stringify($scope.viewedListings);
+          }
+        }
+      } else { //they haven't viewed any yet, add this to the arr
+      $scope.viewedListings = [$scope.mls];
+      localStorage.inhouseViewedListings = JSON.stringify($scope.viewedListings);
+    }
+  }
 }
 
 inhouseApi.newApi.getListingDetails($scope.mls, 'v2').success(function(response) {
-	if(typeof response.listing !== 'undefined') {
+  if(typeof response.listing !== 'undefined') {
 
-		$scope.listing = response.listing;
-		$scope.listing.showOldTable = true;
+    $scope.listing = response.listing;
+    $scope.listing.showOldTable = true;
 
-		if($scope.listing.photos !== undefined) {
-			var photos = $scope.listing.photos;
-		} else {
-			var photos = [];
-		}
+    if($scope.listing.photos !== undefined) {
+      var photos = $scope.listing.photos;
+    } else {
+      var photos = [];
+    }
 
-	} else if(typeof response.data !== 'undefined' && typeof response.data.listing !== 'undefined') {
-		$scope.listing = response.data.listing;
-		var photos = $scope.listing.Details.photos;
-	} else {
-		$scope.listing = response;
-	}
+  } else if(typeof response.data !== 'undefined' && typeof response.data.listing !== 'undefined') {
+    $scope.listing = response.data.listing;
+    var photos = $scope.listing.Details.photos;
+  } else {
+    $scope.listing = response;
+  }
 
-	$scope.url = window.location.href;
-	$scope.lightBox = [];
+  $scope.url = window.location.href;
+  $scope.lightBox = [];
 
-	for (var i = 0; i < photos.length; i++) {
-		$scope.lightBox.push({
-			'index': i,
-			'source' : photos[i].large,
-			'type' : 'image',
-			'caption' : photos[i].caption
-		});
-	}
+  for (var i = 0; i < photos.length; i++) {
+    $scope.lightBox.push({
+      'index': i,
+      'source' : photos[i].large,
+      'type' : 'image',
+      'caption' : photos[i].caption
+    });
+  }
 
-	$timeout(function() {
-		$scope.$broadcast('listingLoaded', $scope.listing);
-		$scope.$on('owlItemClicked', function(event, args) {
-			$('#listingSlider').carousel(args.index);
-		});
+  $timeout(function() {
+    $scope.$broadcast('listingLoaded', $scope.listing);
+    $scope.$on('owlItemClicked', function(event, args) {
+      $('#listingSlider').carousel(args.index);
+    });
 
-		$('#main-view').removeClass('load-overlay');
-	});
+    $('#main-view').removeClass('load-overlay');
+  });
 });
 };
 
--- dev/is/controllers/property-story-controller.js
+++ dev/is/controllers/property-story-controller.js
@@ -1,31 +1,31 @@
 angular.module('ihframework')
 .controller('propertyStoryView', ['$window', '$rootScope', '$scope', '$routeParams', 'inhouseApi', '$timeout', function($window, $rootScope, $scope, $route, inhouseApi, $timeout) {
-	$('#main-view').addClass('load-overlay');
+  $('#main-view').addClass('load-overlay');
 
-	if(typeof $rootScope.theWebsiteData.listingConfig !== 'undefined' && $rootScope.theWebsiteData.listingConfig === 's2') {
-		$rootScope.$broadcast('hideFooter');
-		$scope.$on('$destroy', function() {
-			$rootScope.$broadcast('showFooter');
-		});
-	}
+  if(typeof $rootScope.theWebsiteData.listingConfig !== 'undefined' && $rootScope.theWebsiteData.listingConfig === 's2') {
+    $rootScope.$broadcast('hideFooter');
+    $scope.$on('$destroy', function() {
+      $rootScope.$broadcast('showFooter');
+    });
+  }
 
-	$scope.$on('$destroy', function() {
-		//prevent the modal from persisting
-		$('#accountModal').off('hidden.bs.modal');
-		$('#accountModal').modal('hide');
-	});
-	// not sure how to access shareUrl in components / possibly not needed here?; see inhouse.navbar.js
-	$scope.shareUrl = "https://www.getinhouse.io/share-listing/" + $rootScope.theUserData.userId + '/' + $rootScope.theWebsiteData.mls;
-	console.log($scope.shareUrl);
-	inhouseApi.newApi.getListingDetails($rootScope.theWebsiteData.mls, 'v2').success(function(response) {
-		if(response.listing !== undefined) $scope.listing = response.listing;
-		else $scope.listing = response.data.listing;
-	});
+  $scope.$on('$destroy', function() {
+    //prevent the modal from persisting
+    $('#accountModal').off('hidden.bs.modal');
+    $('#accountModal').modal('hide');
+  });
+  // not sure how to access shareUrl in components / possibly not needed here?; see inhouse.navbar.js
+  $scope.shareUrl = "https://app.getinhouse.io/user/" + $rootScope.theUserData.userId + '/share-listing/' + $rootScope.theWebsiteData.mls;
+  console.log($scope.shareUrl);
+  inhouseApi.newApi.getListingDetails($rootScope.theWebsiteData.mls, 'v2').success(function(response) {
+    if(response.listing !== undefined) $scope.listing = response.listing;
+    else $scope.listing = response.data.listing;
+  });
 
-	$scope.showLightBox = function(index) {
-		var firstHalf = $scope.lightBox.slice().slice(0, index - 1);
-		var secondHalf = $scope.lightBox.slice().slice(index);
+  $scope.showLightBox = function(index) {
+    var firstHalf = $scope.lightBox.slice().slice(0, index - 1);
+    var secondHalf = $scope.lightBox.slice().slice(index);
 
-		UIkit.lightbox.create(secondHalf.concat(firstHalf)).show();
-	};
+    UIkit.lightbox.create(secondHalf.concat(firstHalf)).show();
+  };
 }]);
